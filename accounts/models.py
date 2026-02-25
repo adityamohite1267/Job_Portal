a971c2b065c5187aa5355dce8cc62b90b2f9fda7
adityamohite1267 <mohiteaditya7777@gmail.com>
2026-02-25 17:35:42 +0530

    creating models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+
+    USER_TYPE_CHOICES = (
+        ('jobseeker', 'Job Seeker'),
+        ('recruiter', 'Recruiter'),
+    )
+
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
+
+    def __str__(self):
+        return self.username
