406a36bd0900f201157e3a864d1e574da93f0017
adityamohite1267 <mohiteaditya7777@gmail.com>
2026-02-26 22:10:26 +0530

    Day 2: Added RecruiterProfile, JobSeekerProfile, Skill model with auto profile creation using signals

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser,RecruiterProfile,JobSeekerProfile,Skill
 
 
 @admin.register(CustomUser)
@@ -12,9 +12,7 @@ class CustomUserAdmin(UserAdmin):
 
         fieldsets.append(
             ("Additional Info", {
-                "fields": ("user_type", "phone", "profile_picture"),
-            })
-        )
+                "fields": ("user_type", "phone", "profile_picture"),}))
 
         return fieldsets
 
@@ -23,8 +21,24 @@ class CustomUserAdmin(UserAdmin):
 
         fieldsets.append(
             ("Additional Info", {
-                "fields": ("user_type", "phone", "profile_picture"),
-            })
-        )
+                "fields": ("user_type", "phone", "profile_picture"),}))
 
         return fieldsets
+
+@admin.register(RecruiterProfile)
+class RecruiterProfileAdmin(admin.ModelAdmin):
+    list_display = ("user","company_name","created_at")
+    search_fields = ("company_name","user__username","user__email")
+    list_filter = ("created_at",)# here use comma because it is a tuple not string
+
+@admin.register(JobSeekerProfile)
+class JobSeekerProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "experience", "education", "created_at")
+    search_fields = ("user__username", "user__email", "education")
+    list_filter = ("experience", "created_at")
+    filter_horizontal = ("skills",) # it is ManyToMany UI improvement
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
--- accounts/models.py
+++ accounts/models.py
@@ -1,32 +1,50 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.conf import settings
 
 class CustomUser(AbstractUser):
 
-    USER_TYPE_CHOICES = (
-        ('jobseeker', 'Job Seeker'),
-        ('recruiter', 'Recruiter'),
-    )
-
-    user_type = models.CharField(
-        max_length=20,
-        choices=USER_TYPE_CHOICES,
-    )
-
-    phone = models.CharField(
-        max_length=15,
-        blank=True,
-        null=True
-    )
-
-    profile_picture = models.ImageField(
-        upload_to='profiles/',
-        blank=True,
-        null=True
-    )
-
+    USER_TYPE_CHOICES = (('jobseeker', 'Job Seeker'),('recruiter', 'Recruiter'),)
+    email = models.EmailField(unique=True)
+    user_type = models.CharField(max_length=20,choices=USER_TYPE_CHOICES,)
+    phone = models.CharField(max_length=15,blank=True,null=True)
+    profile_picture = models.ImageField(upload_to='profiles/',blank=True,null=True)
     REQUIRED_FIELDS = ['email', 'user_type']
 
     def __str__(self):
         return f"{self.username} ({self.user_type})"
+    
+    class Meta:
+        verbose_name = "Custom User"
+        verbose_name_plural = "Custom Users"
+
+# create seprate profiles models to avoid messiness in customuser 
+class RecruiterProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=200)
+    company_website = models.URLField(blank=True,null=True)
+    company_description = models.TextField(blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.company_name
+    
+#skill model will help in jobseekerprofile  
+class Skill(models.Model):
+    name = models.CharField(max_length=100,unique=True)
+    def __str__(self):
+        return self.name
+
+class JobSeekerProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    resume = models.FileField(upload_to='resumes/',blank=True,null=True)
+    skills = models.ManyToManyField(Skill, blank=True)
+    experience = models.IntegerField(default=0)
+    education = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
+    
