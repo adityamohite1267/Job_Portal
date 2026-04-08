4edb164a64da27cd5f8444a812198f05d3412d84
adityamohite1267 <mohiteaditya7777@gmail.com>
2026-04-08 18:10:40 +0530

    Optimized application status email notifications by preventing duplicate emails using pre_save signal and status-change detection logic

--- applications/signals.py
+++ applications/signals.py
@@ -1,32 +1,33 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
 from .models import Application
 
-@receiver(post_save,sender=Application)
+@receiver(pre_save,sender=Application)
 def send_status_email(sender,instance,**kwargs):
 
+    if not instance.pk:
+        return
+    previous = Application.objects.get(pk=instance.pk)
+    if previous.status == instance.status:
+        return
+    
     if instance.status == "SHORTLISTED":
-
         send_mail(
             subject="Application Shortlisted ",
-            message=f"Congratulations {instance.user.username}, your application for {instance.job.title} hs been shortlisted.",
+            message=f"Congratulations {instance.applicant.username}, your application for {instance.job.title} has been shortlisted.",
             from_email="mohiteaditya7777@gmail.com",
-            recipient_list=[instance.user.email],
-        )
+            recipient_list=[instance.applicant.email],)
     elif instance.status == "REJECTED":
         send_mail(
             subject="Application Update",
-            message=f"Hello {instance.user.username}, unfortunately your application for {instance.job.title} was not selected.Better Luck next time ",
+            message=f"Hello {instance.applicant.username}, unfortunately your application for {instance.job.title} was not selected.Better Luck next time ",
             from_email="mohiteaditya7777@gmail.com",
-            recipient_list=[instance.user.email],
-        )
-
+            recipient_list=[instance.applicant.email],)
     elif instance.status == "HIRED":
-        send_mail(subject="Congratulations! You're Selected ",
-            message=f"Great news {instance.user.username}! You have been hired for {instance.job.title}.",
+        send_mail(
+            subject="Congratulations! You're Selected ",
+            message=f"Great news {instance.applicant.username}! You have been hired for {instance.job.title}.",
             from_email="mohiteaditya7777@gmail.com",
-            recipient_list=[instance.user.email],
-
-        )
+            recipient_list=[instance.applicant.email],)
     
