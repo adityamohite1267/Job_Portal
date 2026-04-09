b55e0cb6acc2a65e898f77fcedf20abbc8f9238f
adityamohite1267 <mohiteaditya7777@gmail.com>
2026-04-09 23:32:57 +0530

    enable auto superuser creation

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ import os
 import dj_database_url
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 from pathlib import Path
@@ -82,11 +83,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
-    )
-}
+if os.getenv("DATABASE_URL"):
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.getenv("DATABASE_URL")
+        )
+    }
+else:
+    DATABASES = {
+        "default":{
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
